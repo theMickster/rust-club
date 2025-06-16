29ce7b943d1dccd077ec196efeab9725874a2687
Mick Letofsky <mick.tosk@gmail.com>
2025-06-16 13:56:39 -0600

    Implement remaining simple/trivial operators. Practice Rust unit testing.

--- proyectos-tontos/02-calculator/src/main.rs
+++ proyectos-tontos/02-calculator/src/main.rs
@@ -6,10 +6,36 @@ fn main() {
     let mut calc01: Calculator = Calculator::new("Texas Instruments", "TI-83 Plus");
     println!("{}", calc01.display());
     println!("Calculator 01 Total: {}", calc01.get_total().to_string());
+    
     calc01.add(5);
     println!("Calculator 01 Total: {}", calc01.get_total().to_string());
+    
     calc01.add(15);
     println!("Calculator 01 Total: {}", calc01.get_total().to_string());
+    
+    calc01.clear();
+    println!("Calculator 01 Total: {}", calc01.get_total().to_string());    
+
+    calc01.add(5);
+    calc01.add(15);
+    calc01.subtract(3);
+    println!("Calculator 01 Total: {}", calc01.get_total().to_string());
+
+    calc01.multiply(4);
+    println!("Calculator 01 Total: {}", calc01.get_total().to_string());
+
+    let result = calc01.divide(2);
+    match result {
+        Ok(_) => println!("Calculator 01 Total: {}", calc01.get_total().to_string()),
+        Err(e) => println!("Division failed: {}", e),
+    }
+
+    let result = calc01.divide(0);
+    match result {
+        Ok(_) => println!("Calculator 01 Total: {}", calc01.get_total().to_string()),
+        Err(e) => println!("Division failed: {}", e),
+    }
+        
     calc01.clear();
     println!("Calculator 01 Total: {}", calc01.get_total().to_string());    
 }
